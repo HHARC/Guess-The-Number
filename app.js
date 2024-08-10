13bf114d775383cd61205d7965d8d98011316ef0
HHARC <hharc123@gmail.com>
2024-08-10 04:33:29 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+"use strict";
+let score = 20;
+let highscore = 0;
+let number = Math.trunc(Math.random() * 20 + 1);
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  if (!guess) {
+    document.querySelector(".message").textContent = "No Content";
+  } else if (guess === number) {
+    document.querySelector(".number").textContent = number;
+    document.querySelector(".message").textContent =
+      "Congratulations! You Guess the Number Correctly🎉";
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
+  } else if (guess > number) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too High📈";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You Lose the Game🙃";
+      document.querySelector(".score").textContent = 0;
+    }
+  } else if (guess < number) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too Low📉";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You Lose the Game🙃";
+      document.querySelector(".score").textContent = 0;
+    }
+  }
+});
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  document.querySelector(".score").textContent = score;
+  number = Math.trunc(Math.random() * 20 + 1);
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = " ";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
+});
